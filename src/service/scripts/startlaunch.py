ae74010401995f6472366925fc7b96604fd35484
ouyangvage <ouyangvage@sina.com>
2022-02-16 17:08:14 +0800

    add service

--- src/service/scripts/startlaunch.py
+++ src/service/scripts/startlaunch.py
@@ -1,24 +1,50 @@
 import rospy
 import roslaunch
+from service.srv import MapServer, MapServerResponse
 
-# def startlaunch():
-#     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-#     roslaunch.configure_logging(uuid)
-#     tracking_launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/fdata/ros/src/model/launch/robot.launch"])
-#     tracking_launch.start()
-#     while not rospy.is_shutdown():
-#         tracking_launch.shutdown()
+command = ''
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/fdata/ros/src/model/launch/robot.launch"])
+
+def map_function(req):
+    global command 
+    print("Returning request:%s"%(req.map_controller))
+    if req.map_controller == 'map_create':
+        command = 'map_create'
+        print("command:%s"%(command))
+        return MapServerResponse('map create')
+    elif req.map_controller == 'map_save':
+        command = 'map_save'
+        print("command:%s"%(command))
+        return MapServerResponse('map_save')
+    elif req.map_controller  == 'map_load':
+        return MapServerResponse('map_load')
+    
+
+def map_server():
+    global launch
+    global command
+    rospy.init_node('map_server')
+    server = rospy.Service('map_server', MapServer, map_function)
+    print("callback server:%s"%(server))
+    print("Ready to start map server.")
+    while(1):
+        print("command:%s"%(command))
+        if command == 'map_create':
+            print("callback server:map_create")
+            launch.start()
+            command = ''
+        elif command == 'map_save':
+            print("callback servermap_save")
+            launch.shutdown()
+            command = ''
+            break
+    rospy.spin()
     
 
 if __name__ == "__main__":
     try:
-        rospy.init_node('en_Mapping', anonymous=True)
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
-        launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/fdata/ros/src/model/launch/robot.launch"])
-        launch.start()
-        rospy.loginfo("started")
-        while not rospy.is_shutdown():
-            pass
+        map_server()
     except rospy.ROSException:
         pass
