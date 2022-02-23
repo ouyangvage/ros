1a9cb13c59d66ceed4e9c240f5bee13a4b4e4d91
ouyangvage <ouyangvage@sina.com>
2022-02-23 15:48:35 +0800

    get robot pose

--- src/navigation/scripts/getpose.py
+++ src/navigation/scripts/getpose.py
@@ -1,16 +1,21 @@
 import rospy
 import tf
 import time
+from std_msgs.msg import String
 
 if __name__ == '__main__':
     rospy.init_node('robot_tf_listener')
     listener = tf.TransformListener() #start listener
+    pub = rospy.Publisher("/current_pose", String, queue_size=10)
+    msg = String()
     while not rospy.is_shutdown():
         try:
             #获取 base_footprint 相对 map 的坐标变换，返回两个列表
-            (position, orient) = listener.lookupTransform('map', 'base_footprint', rospy.Time(0))
+            (position, orientation) = listener.lookupTransform('map', 'base_footprint', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
         print('position', position)
-        print('orient', orient)
+        print('orientation', orientation)
+        msg.data = 'position:' + str(position) +' ' + 'orientation:' + str(orientation)
+        pub.publish(msg)
         time.sleep(0.5)
